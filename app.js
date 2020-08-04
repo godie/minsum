fbe54f9eddbea900daa8df91f52c83da7804e419
Diego Mendoza <godie@notitec.info>
2020-08-04 00:16:06 -0500

    measuring time.

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var msg = 'Hello World';
 console.log(msg);
 const app = {};
 
-app.minSum = (num, k)  => {
+function minSum(num, k){
+
     // Write your code here
     const sorted = num.sort((a,b) => parseInt(a) - parseInt(b));
     while(k-- > 0 ){
         let max = sorted[0];
-       // console.log('max',max);
+        //console.log('k',k, 'max',max);
+        if(max == 1 && k < num.length) break;
         num.splice(0,1);
         let replaced = Math.ceil(max/2);
         let inserted = false;
@@ -17,11 +19,12 @@ app.minSum = (num, k)  => {
                // console.log(i, replaced);
                 num.splice(i,0, replaced);
                 inserted = true;
-                while(num.indexOf(max) > -1 ){
+                //while(num.indexOf(max) > -1 ){
+                while(num[0] === max){
                     k--;
                     //console.log('=======',k)
                     if(k < 1 ) break;
-                    let newMax = sorted[0];
+                    //let newMax = sorted[0];
                     //console.log('newMax',newMax);
                     num.splice(0,1);
                     num.splice(i,0, replaced);
@@ -32,7 +35,12 @@ app.minSum = (num, k)  => {
         }
 
     }
+  
     return num.reduce((a,b) => a+b, 0);
 }
+
+app.minSum = minSum;
+
+
 
 module.exports = app;
